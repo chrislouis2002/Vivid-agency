f4e502d20b652c953863c813f894fb6a43f6d41d
Chris Louis <chrislouis2002@gmail.com>
2026-03-07 14:55:59 +0100

    public

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -1,4 +1,4 @@
-import VideoThumb from "@/public/images/vivid-hero.jpg";
+import VideoThumb from "@/images/vivid-hero.jpg";
 import ModalVideo from "@/components/modal-video";
 
 export default function HeroHome() {
